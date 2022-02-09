101f539fa99ad0dd3a5732bb8d4de2dfb8a93c2d
Ryan Ashby <rjapsu160@gmail.com>
2022-02-09 10:41:35 -0800

    add getCharacter function to index.js
    Co-authored-by: Jase Seeley <jcseeley31590@yahoo.com>
    co-authored-by: Riley Shimp <rilryshimp@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Superhero from './super';
 import getRandomInt from './number';
+
+function getCharacter(response) {
+  if (response.response === "success") {
+    $('#').text(`Name: ${response.name}`);
+    $('#').text(`Alignment: ${response.biography.alignment}`);
+    $('#').html(`<img src="${response.image.url}">`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.error}`)
+  }
+}
