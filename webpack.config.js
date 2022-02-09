767abe0b23b71cf003887b1db60a0cd4c20dc30f
Ryan Ashby <rjapsu160@gmail.com>
2022-02-09 10:02:41 -0800

    initial commit with basic template
    co-authored-by: Riley Shimp <rilryshimp@gmail.com>
    Co-authored-by: Jase Seeley <jcseeley31590@yahoo.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -16,11 +17,12 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Euler',
+      title: 'template',
       template: './src/index.html',
       inject: 'body'
     }),
-    new ESLintPlugin()
+    new ESLintPlugin(),
+    new Dotenv()
   ],
   module: {
     rules: [
